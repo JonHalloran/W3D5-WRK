01ce906987f820007c6bcaf23470f14bb483c2ef
JonHalloran <angmar139@hotmail.com>
2018-03-02 18:00:19 -0800

    Find out bad at metaprogramming

--- AR 1/lib/00_attr_accessor_object.rb
+++ AR 1/lib/00_attr_accessor_object.rb
@@ -6,11 +6,9 @@ class AttrAccessorObject
       var_sym = var.to_sym
       var_equal_sym = "#{var}=".to_sym
       var_instance = "@#{var}".to_sym
-      var_ins_set = "@#{var}=".to_sym
 
       define_method(var_sym) { instance_variable_get(var_instance) }
       define_method(var_equal_sym) { |given| instance_variable_set(var_instance, given)}
-      # define_method()
     end
   end
 end
--- AR 1/lib/01_sql_object.rb
+++ AR 1/lib/01_sql_object.rb
@@ -5,25 +5,59 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
+    return @columns if @columns
+
+    columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    @columns = columns.first.map!(&:to_sym)
+    @columns
     # ...
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method(column.to_s) do
+        attributes[column]
+      end
+      define_method("#{column}=") do |given|
+        attributes[column] = given
+      end
+    end
+
+
   end
 
   def self.table_name=(table_name)
+
+    @table_name = table_name
     # ...
   end
 
   def self.table_name
     # ...
+      @table_name = self.to_s.underscore + "s"
   end
 
   def self.all
-    # ...
+    hashes = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    parse_all(hashes)
   end
 
   def self.parse_all(results)
+    results.each do |result|
+      Cat.new(result)
+    end
     # ...
   end
 
@@ -32,10 +66,22 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    @params = params
+    class_columns = self.class.columns
+    params.each do |param, value|
+      param = param.to_sym
+      unless class_columns.include?(param)
+        raise "unknown attribute '#{param}'"
+      end
+      self.send(param.to_s +  "=", value)
+    end
   end
 
+
   def attributes
+    @attributes ||= @params
+    @attributes ||= {}
+    @attributes
     # ...
   end
 
